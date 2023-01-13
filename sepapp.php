54157d636e9ab232d1199e8e9d2f8925af5938c1
B. Endres <endres@systopia.de>
2023-01-13 09:35:43 -0600

    [#14] civix upgrade

--- sepapp.php
+++ sepapp.php
@@ -274,7 +274,6 @@ function sepapp_civicrm_postProcess($formName, &$form)
     }
 }
 
-
 /**
  * Will install the SEPA payment processor
  */
@@ -347,7 +346,6 @@ function sepapp_civicrm_apiWrappers(&$wrappers, $apiRequest)
     }
 }
 
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -356,16 +354,6 @@ function sepapp_civicrm_apiWrappers(&$wrappers, $apiRequest)
 function sepapp_civicrm_config(&$config)
 {
     _sepapp_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function sepapp_civicrm_xmlMenu(&$files)
-{
-    _sepapp_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -406,58 +394,6 @@ function sepapp_civicrm_uninstall()
 function sepapp_civicrm_upgrade($op, CRM_Queue_Queue $queue = null)
 {
     return _sepapp_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function sepapp_civicrm_managed(&$entities)
-{
-    _sepapp_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function sepapp_civicrm_caseTypes(&$caseTypes)
-{
-    _sepapp_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function sepapp_civicrm_angularModules(&$angularModules)
-{
-    _sepapp_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function sepapp_civicrm_alterSettingsFolders(&$metaDataFolders = null)
-{
-    _sepapp_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
